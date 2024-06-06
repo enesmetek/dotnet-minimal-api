8e40c364a4d587ee3d80e94ae0f607de2ac3bf06
Enes Mete Kafali <emkafali@gmail.com>
2024-06-06 21:26:00 +0300

    Add project files.

---
+++ Library.Api/Data/DatabaseInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+
+namespace Library.Api.Data
+{
+    public class DatabaseInitializer(IDbConnectionFactory connectionFactory)
+    {
+        private readonly IDbConnectionFactory _connectionFactory = connectionFactory;
+
+        public async Task InitializeAsync()
+        {
+            using var connection = await _connectionFactory.CreateConnectionAsync();
+            await connection.ExecuteAsync(
+                @"CREATE TABLE IF NOT EXISTS Books (
+            Isbn TEXT PRIMARY KEY,
+            Title TEXT NOT NULL,
+            Author TEXT NOT NULL,
+            ShortDescription TEXT NOT NULL,
+            PageCount INTEGER,
+            ReleaseDate TEXT NOT NULL)"
+                );
+        }
+    }
+}
+
+
---
+++ Library.Api/Data/SqliteConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace Library.Api.Data
+{
+    public class SqliteConnectionFactory(string connectionString) : IDbConnectionFactory
+    {
+        private readonly string _connectionString = connectionString;
+
+        public async Task<IDbConnection> CreateConnectionAsync()
+        {
+            var connection = new SqliteConnection(_connectionString);
+            await connection.OpenAsync();
+            return connection;
+        }
+    }
+}
