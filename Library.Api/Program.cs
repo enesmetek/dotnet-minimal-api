99740d0fa7fb356227f4f337bbb1eaa70afe9c9f
Enes Mete Kafali <emkafali@gmail.com>
2024-06-07 09:31:23 +0300

    refactor: structure endpoints

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,9 +1,7 @@
 using FluentValidation;
-using FluentValidation.Results;
 using Library.Api.Auth;
 using Library.Api.Data;
-using Library.Api.Models;
-using Library.Api.Services;
+using Library.Api.Endpoints.Internal;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -30,8 +28,9 @@ var builder = WebApplication.CreateBuilder(args);
 
     builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
         new SqliteConnectionFactory(builder.Configuration.GetConnectionString("Default")!));
+    builder.Services.AddEndpoints<Program>(builder.Configuration);
+
     builder.Services.AddSingleton<DatabaseInitializer>();
-    builder.Services.AddSingleton<IBookService, BookService>();
     builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 }
 
@@ -43,93 +42,9 @@ var app = builder.Build();
     app.UseSwaggerUI();
 
     app.UseAuthentication();
+    app.UseAuthorization();
 
-    app.MapPost("books",
-        //[Authorize(AuthenticationSchemes = ApiKeySchemeConstants.SchemeName)]
-        //[AllowAnonymous]
-        async (Book book, IBookService bookService,
-            IValidator<Book> validator, LinkGenerator linker,
-            HttpContext context) =>
-        {
-            var validationResult = await validator.ValidateAsync(book);
-            if (!validationResult.IsValid)
-            {
-                return Results.BadRequest(validationResult.Errors);
-            }
-
-            var created = await bookService.CreateAsync(book);
-            if (!created)
-            {
-                return Results.BadRequest(new List<ValidationFailure>
-                {
-                    new("Isbn", "A book with this ISBN-13 already exists")
-                });
-            }
-
-            var path = linker.GetPathByName("GetBook", new { isbn = book.Isbn })!;
-            var locationUri = linker.GetUriByName(context, "GetBook", new { isbn = book.Isbn })!;
-            return Results.Created(locationUri, book);
-            //return Results.CreatedAtRoute("GetBook", new { isbn = book.Isbn }, book);
-            //return Results.Created($"/books/{book.Isbn}", book);
-        })
-    .WithName("CreateBook")
-    .Accepts<Book>("application/json")
-    .Produces<Book>(201)
-    .Produces<IEnumerable<ValidationFailure>>(400)
-    .WithTags("Books"); //.AllowAnonymous();
-
-    app.MapGet("books", async (IBookService bookService, string? searchTerm) =>
-    {
-        if (searchTerm is not null && !string.IsNullOrWhiteSpace(searchTerm))
-        {
-            var matchedBooks = await bookService.SearchByTitleAsync(searchTerm);
-            return Results.Ok(matchedBooks);
-        }
-
-        var books = await bookService.GetAllAsync();
-        return Results.Ok(books);
-    })
-    .WithName("GetBooks")
-    .Produces<IEnumerable<Book>>(200)
-    .WithTags("Books");
-
-    app.MapGet("books/{isbn}", async (string isbn, IBookService bookService) =>
-    {
-        var book = await bookService.GetByIsbnAsync(isbn);
-        return book is not null ? Results.Ok(book) : Results.NotFound();
-    })
-    .WithName("GetBook")
-    .Produces<Book>(200)
-    .Produces(404)
-    .WithTags("Books");
-
-    app.MapPut("books/{isbn}", async (string isbn, Book book, IBookService bookService, IValidator<Book> validator) =>
-    {
-        book.Isbn = isbn;
-        var validationResult = await validator.ValidateAsync(book);
-        if (!validationResult.IsValid)
-        {
-            return Results.BadRequest(validationResult.Errors);
-        }
-
-        var updated = await bookService.UpdateAsync(book);
-        return updated ? Results.Ok(book) : Results.NotFound();
-    })
-    .WithName("UpdateBook")
-    .Accepts<Book>("application/json")
-    .Produces<Book>(200)
-    .Produces<IEnumerable<ValidationFailure>>(400)
-    .WithTags("Books");
-
-    app.MapDelete("books/{isbn}", async (string isbn, IBookService bookService) =>
-    {
-        var deleted = await bookService.DeleteAsync(isbn);
-        return deleted ? Results.NoContent() : Results.NotFound();
-    })
-    .WithName("DeleteBook")
-    .Produces(204)
-    .Produces(404)
-    .WithTags("Books");
+    app.UseEndpoints<Program>();
 
     var databaseInitializer = app.Services.GetRequiredService<DatabaseInitializer>();
     await databaseInitializer.InitializeAsync();
